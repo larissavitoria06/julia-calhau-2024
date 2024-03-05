8d54a1111d3501276e8f0fc896ae2486e8931cf8
larissavitoria06 <160656461+larissavitoria06@users.noreply.github.com>
2024-03-05 16:59:23 -0300

    Create bilhões.c

---
+++ bilhões.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	long long int x, y;
+	printf("Digite dois números inteiros:");
+	scanf("%lld %lld", &x, &y);
+	printf("O número %lld multiplicado por 4 bilhões é %lld", x, x * 4000000000LL);
+	printf("O número %lld multiplicado por 4 bilhões é %lld", y, y * 4000000000LL);
+	return 0;
+}
